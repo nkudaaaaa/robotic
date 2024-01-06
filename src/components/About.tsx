9d167827c05a1e25010d6d12d55778c89ffe72ed
Nnen431 <Kamishino1647@gmail.com>
2024-01-06 18:04:41 +0300

    yyyyyyy

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,74 +1,101 @@
-import "../css/About.css"
-// import minecr from "../assets/about/minecraftLogo.svg"
-// import wifi from "../assets/about/wifiLogo.svg"
-// import gear from "../assets/about/gearLogo.svg"
-// import fire from "../assets/about/fireLogo.svg"
-// import puzzle from "../assets/about/puzzle.svg"
-// import hummer from "../assets/about/hammer.svg"
-// import figma from "../assets/about/figma.svg"
-// import lightning from "../assets/about/lightningLogo.svg"
-import bg  from "../assets/about/bg.svg";
-import legoCard from "../assets/about/legoCard.svg"
-import htmlCard from "../assets/about/htmlCard.svg"
-import javaCard from "../assets/about/javaCard.svg"
-import pythonCard from "../assets/about/pythonCard.svg"
-import cplusCard from "../assets/about/cplusCard.svg"
-import cshCard from "../assets/about/cshLogo.svg"
-import unityCard from "../assets/about/unityCard.svg";
-import gamedev from "../assets/about/gamedev.svg"
-import modeling from "../assets/about/3D.svg"
-import websites from "../assets/about/websites.svg"
-import programming from "../assets/about/programming.svg"
-import robots from "../assets/about/robots.svg"
-
-
+import React, { useState, useEffect } from 'react';
+import '../css/About.css';
+import bg from '../assets/about/bg.svg';
+import legoCard from '../assets/about/legoCard.svg';
+import htmlCard from '../assets/about/htmlCard.svg';
+import javaCard from '../assets/about/javaCard.svg';
+import pythonCard from '../assets/about/pythonCard.svg';
+import cplusCard from '../assets/about/cplusCard.svg';
+import cshCard from '../assets/about/cshLogo.svg';
+import unityCard from '../assets/about/unityCard.svg';
+import gamedev from '../assets/about/gamedev.svg';
+import modeling from '../assets/about/3D.svg';
+import websites from '../assets/about/websites.svg';
+import programming from '../assets/about/programming.svg';
+import robots from '../assets/about/robots.svg';
 
 const About = () => {
-    return (
-        <div className="about" id="about">
-            <div className="container-main">
-                <span className="main-sign-about">Добро пожаловать в Роботик— ваш ключ к захватывающему миру технологий и творчества!{<br />}{<br />}
+  const [animatedDirection, setAnimatedDirection] = useState<string>('');
+  const [directionIndex, setDirectionIndex] = useState<number>(0);
 
-                    Открывайте для себя веселое программирование, захватывающие проекты и уникальные курсы, которые не только раскроют ваши таланты, но и повысят успеваемость в школе!
-                    Превратите свою любовь к технике в потрясающий опыт обучения с Роботик— где будущее начинается сегодня!
-                </span>
-                <span className="main-sign-about" id="span-hashtags">#Роботик #Технотворчество #ПрограммированиеДляДетей</span>
-                <span className="main-sign-about" id="span-education">ПРОГРАММА ОБУЧЕНИЯ</span>
+  const directionsOrder = [1, 4, 7, 5, 6, 2, 3, 7, 1, 3, 4, 6, 3, 5];
+  const directions = [
+    legoCard,
+    htmlCard,
+    javaCard,
+    pythonCard,
+    cplusCard,
+    cshCard,
+    unityCard,
+  ];
 
-                <div className="line" />
-                <div className="images-bg">
-                    <img src={bg} id="about-bg" />
-                </div>
-                <span className="main-sign-about" id="span-program">НАПРАВЛЕНИЯ ДЛЯ ИЗУЧЕНИЯ</span>
-                <div className="cards-directions">
-                    <div className="upper-line">
-                        <img src={legoCard} className="directions-item" />
-                        <img src={htmlCard} alt="" className="directions-item" />
-                        <img src={javaCard} alt="" className="directions-item" />
-                        <img src={pythonCard} alt="" className="directions-item" />
-                    </div>
-                    <div className="down-line">
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      const currentDirection = directionsOrder[directionIndex];
+      setAnimatedDirection(directions[currentDirection - 1]);
 
-                        <img src={cplusCard} className="directions-item" />
-                        <img src={cshCard} className="directions-item" />
-                        <img src={unityCard} className="directions-item" />
-                    </div>
-                </div>
-                <span className="main-sign-about" id="span-directions">ЧЕМУ МЫ ОБУЧАЕМ</span>
-                <div className="cards-education">
-                    <div className="upper-directions">
-                        <img src={gamedev} alt="" />
-                        <img src={modeling} alt="" />
-                        <img src={websites} alt="" />
-                    </div>
-                    <div className="upper-directions">
-                        <img src={programming} alt="" />
-                        <img src={robots} alt="" id="robototechnics" />
-                    </div>
-                </div>
-            </div>
+      // Increment the directionIndex for the next interval
+      setDirectionIndex((prevIndex) => (prevIndex + 1) % directionsOrder.length);
+    }, 1500);
+
+    return () => clearInterval(intervalId);
+  }, [directionsOrder, directionIndex]);
+
+  // Разделение массива направлений на две строки
+  const firstRow = directions.slice(0, Math.ceil(directions.length / 2));
+  const secondRow = directions.slice(Math.ceil(directions.length / 2));
+
+  return (
+    <div className="about" id="about">
+      <div className="container-main">
+        <span className="main-sign-about">Добро пожаловать в Роботик— ваш ключ к захватывающему миру технологий и творчества!{<br />}{<br />}
+          Открывайте для себя веселое программирование, захватывающие проекты и уникальные курсы, которые не только раскроют ваши таланты, но и повысят успеваемость в школе! Превратите свою любовь к технике в потрясающий опыт обучения с Роботик— где будущее начинается сегодня!
+        </span>
+        <span className="main-sign-about" id="span-hashtags">#Роботик #Технотворчество #ПрограммированиеДляДетей</span>
+        <span className="main-sign-about" id="span-education">ПРОГРАММА ОБУЧЕНИЯ</span>
+
+        <div className="line" />
+        <div className="images-bg">
+          <img src={bg} id="about-bg" alt="Background" />
         </div>
-    )
-}
+        <span className="main-sign-about" id="span-program">НАПРАВЛЕНИЯ ДЛЯ ИЗУЧЕНИЯ</span>
+        <div className="cards-directions">
+          <div className="upper-line">
+            {firstRow.map((direction, index) => (
+              <img
+                key={index}
+                src={direction}
+                className={`directions-item ${animatedDirection === direction ? 'animated' : ''}`}
+                alt={`Direction ${index + 1}`}
+              />
+            ))}
+          </div>
+          <div className="down-line">
+            {secondRow.map((direction, index) => (
+              <img
+                key={index}
+                src={direction}
+                className={`directions-item ${animatedDirection === direction ? 'animated' : ''}`}
+                alt={`Direction ${firstRow.length + index + 1}`}
+              />
+            ))}
+          </div>
+        </div>
+        <span className="main-sign-about" id="span-directions">ЧЕМУ МЫ ОБУЧАЕМ</span>
+        <div className="cards-education">
+          <div className="upper-directions">
+            <img src={gamedev} alt="Game Development" />
+            <img src={modeling} alt="3D Modeling" />
+            <img src={websites} alt="Websites" />
+          </div>
+          <div className="upper-directions">
+            <img src={programming} alt="Programming" />
+            <img src={robots} alt="Robotics" id="robototechnics" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default About;
